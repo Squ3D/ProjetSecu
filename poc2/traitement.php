976fece67d43db59304e0296b8a21ae0ad92ab8b
squ3d <atak.main@gmail.com>
2022-01-07 22:21:16 +0100

    Ajout du Poc2
    Génération de signature Xss

---
+++ poc2/traitement.php
@@ -0,0 +1,21 @@
+<Doctype html>
+<meta charset="utf-8">
+<title>Treatment of Ocr output</title>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+    <script type="text/javascript">
+	$.ajax({
+              url:"http://localhost:8080/pepsi/Images/",
+              type: "GET",
+              success: function (data) {
+                   alert(data)
+              }
+           });
+
+	alert("test");
+
+</script>
+
+
